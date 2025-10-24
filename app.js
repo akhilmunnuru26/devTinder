4cb8716416f8f729c5d29d55a83edb225d56c75f
akhilmunnuru26 <munnuruakhil26@gmail.com>
2025-10-24 13:40:51 +0530

    Added Sanity Checks for Patch API

--- app.js
+++ app.js
@@ -1,7 +1,151 @@
 const express = require("express");
 const app = express()
 const connectDB = require('./src/config/database')
+const User = require('./src/models/user');
+app.use(express.json());
+
+
+
+
 const port = 7777;
+
+//Signup API
+
+
+
+app.post("/signup", async (req,res) => {
+    // Create an Instance and save the data in the database as documents
+    console.log("Req Body",req.body)
+    const user = new User(req.body)
+    try{
+        await user.save()
+        res.send("User created successfully");
+    }catch(err){
+        console.log("Error while Saving the data in database");
+        res.send(err.message)
+    }
+})
+
+
+
+//FEED API - GET API - Get All the users
+
+app.get("/feed", async (req,res) => {
+    const users = User.find({})
+    try{
+        const users = await User.find({})
+        res.send(users)
+    }catch(e){
+        res.status(500).send("Something went wrong")
+    }
+})
+
+
+// Get Specific User based on user id
+app.get("/user",  async (req,res) => {
+    const userId = req.body.userId
+    
+    try{
+         const user = await User.findById({ _id:userId })
+         if (!user){
+            res.status(404).send("User not found")
+         }else{
+            res.send(user)
+         }
+         
+
+    }catch(e){
+         res.status(500).send("Something went wrong")
+    }
+   
+})
+
+//Get Specific User based on user email
+// app.get("/user",  async (req,res) => {
+//     const userEmail = req.body.emailId
+//     console.log("Email Id",userEmail)
+  
+//     try{
+//          const user = await User.findById({ emailId: userEmail })
+//          if (!user){
+//             res.status(404).send("User not found")
+//          }else{
+//             res.send(user)
+//          }
+         
+
+//     }catch(e){
+//          res.status(500).send("Something went wrong")
+//     }
+   
+// })
+
+
+
+// Delete User
+
+
+app.delete("/user", async(req,res) => {
+    try{
+        const userId = req.body.userId;
+        const user = await User.findByIdAndDelete({ _id: userId});
+        res.send("User Deleted Successfully");
+    }catch(e){
+        res.status(500).send("Something went wrong")
+    }
+})
+
+
+//API-Patch:  Update User
+
+app.patch("/user/:userId", async(req,res) => {
+    try{
+        const userId = req.params?.userId;
+        const data = req.body
+
+        const ALLOWED_UPDATES = ["photoUrl","bio","skills","gender"];
+        const is_allowed_updates = Object.keys(data).every((key) => ALLOWED_UPDATES.includes(key));
+
+        if (!is_allowed_updates){
+            throw new Error("Update not allowed");
+        }
+
+        if(data?.skills.length > 10){
+            throw new Error("Cannot add skills more than 10");
+        }
+
+        await User.findByIdAndUpdate({ _id: userId}, userObj,{returnDocument: "after",runValidators:true});
+        res.send("User updated Successfully");
+
+    }catch(e){
+        res.status(500).send("Update failed: " + e.message);
+    }
+})
+
+
+
+// update user by email
+
+// app.patch("/user/:userId", async(req,res) => {
+//     try{
+//         const userEmail = req.body.emailId;
+//         const userObj = req.body;
+//         const ALLOWED_UPDATES = ["photoUrl","bio","skills","gender"];
+//         const is_allowed_updates = Object.keys(userObj).every((key) => ALLOWED_UPDATES.includes(key));
+//         if (!is_allowed_updates){
+//             throw new Error("Update not allowed");
+//         }
+//         if (userObj?.skills > 10){
+//             throw new Error("Skills cannot be more than 10");
+//         }
+//         console.log("Patch by Email",userEmail,userObj);
+//         const user = await User.findOneAndUpdate({ emailId: userEmail}, userObj, {returnDocument: "after",runValidators:true});
+//        res.send(user)
+//     }catch(err){
+//        res.status(500).send("Update failed: "+ err.message); 
+//     }
+// })
+
 
 connectDB().then(() => {
     console.log("Database Connection Established Successfully");
---
+++ src/models/user.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+    {
+    firstName:{
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 50,
+    },
+    lastName:{
+        type:String,
+        minLength: 3,
+        maxLength: 50,
+    },
+    emailId: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+//         match: [
+//     /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+//     "Please enter a valid email address"
+//   ],
+        validate(value){
+            const isValidEmail = validator.isEmail(value)
+            if (!isValidEmail){
+                throw new Error("");
+            }
+        }      
+    },
+    password: {
+        type:String,
+        required: true,
+    },
+    age: {
+        type:Number,
+        required: true,
+        min: 18,
+    },
+    gender: {
+        type: String,
+        required: true,
+        validate(value) {
+           if([!"male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid")
+           } 
+        } 
+    },
+    photoUrl: {
+        type: String,
+        default: "https://imgs.search.brave.com/_xa-9DpUwAMeOS95RlsedIc3gvNOQ1A3wRMyGMRctrc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzE1LzU0LzM0LzEw/LzM2MF9GXzE1NTQz/NDEwOTlfbUdBM2lZ/TVlzc05ZYXhXQ09z/TFNJZFdCRlY3SUli/SWIuanBn"
+    },
+    bio: {
+        type: String,
+        default: "This is the default nio of the user"
+    },
+    skills:{
+        type: [String],
+        
+    }
+},{timestamps:true}
+)
+
+const User = mongoose.model("User",userSchema);
+module.exports = User
